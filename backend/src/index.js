80fcaf1268b3829e2148c4a2f7db1cd03710ab7c
Karen Gomes <kngs@ic.ufal.br>
2021-08-15 15:09:26 -0300

    http methods and using insomnia

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,37 @@ const app = express();
 
 app.get('/projects', (request, response) => {
     // return response.send('Hello World'); // enviar como string
-    return response.json({
-        message: 'Hello, Dev'
-    })
+    return response.json([
+        'Project 1',
+        'Project 2',
+        'Project 3'
+    ])
+})
+
+app.post('/projects', (request, response) => {
+    return response.json([
+        'Project 1',
+        'Project 2',
+        'Project 3',
+        'Project 4'
+    ])
+})
+
+app.put('/projects', (request, response) => {
+    return response.json([
+        'Project 5',
+        'Project 2',
+        'Project 3',
+        'Project 4'
+    ])
+})
+
+app.delete('/projects', (request, response) => {
+    return response.json([
+        'Project 5',
+        'Project 2',
+        'Project 4'
+    ])
 })
 
 app.listen(3333, () => {
