6d0bb430c0234ec5977925544ad838e4fa03cf53
Karen Gomes <kngs@ic.ufal.br>
2021-08-15 16:33:35 -0300

    request params

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
 app.get('/projects', (request, response) => {
     // return response.send('Hello World'); // enviar como string
+    const { title } = request.query;
+    console.log(title)
     return response.json([
         'Project 1',
         'Project 2',
@@ -12,6 +15,10 @@ app.get('/projects', (request, response) => {
 })
 
 app.post('/projects', (request, response) => {
+    const body = request.body;
+
+    console.log(body)
+
     return response.json([
         'Project 1',
         'Project 2',
@@ -20,7 +27,10 @@ app.post('/projects', (request, response) => {
     ])
 })
 
-app.put('/projects', (request, response) => {
+app.put('/projects/:id', (request, response) => {
+    const { id } = request.params;
+    console.log(id)
+
     return response.json([
         'Project 5',
         'Project 2',
@@ -29,7 +39,19 @@ app.put('/projects', (request, response) => {
     ])
 })
 
-app.delete('/projects', (request, response) => {
+app.put('/projects/:id/owner/:owner_id', (request, response) => {
+    const params = request.params;
+    console.log(params)
+
+    return response.json([
+        'Project 5',
+        'Project 2',
+        'Project 3',
+        'Project 4'
+    ])
+})
+
+app.delete('/projects/:id', (request, response) => {
     return response.json([
         'Project 5',
         'Project 2',
