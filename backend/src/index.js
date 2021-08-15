e471894eee3cb77abb12dd024087d5afc166e5f7
Karen Gomes <kngs@ic.ufal.br>
2021-08-15 14:19:15 -0300

    first step with node

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/projects', (request, response) => {
+    // return response.send('Hello World'); // enviar como string
+    return response.json({
+        message: 'Hello, Dev'
+    })
+})
+
+app.listen(3333, () => {
+    console.log('Backend started!');
+});
+
+
