0eea4cf59b4d5997dcbd55e15b87c992d8aec29d
Karen Gomes <kngs@ic.ufal.br>
2021-08-19 13:45:33 -0300

    create first migration

---
+++ node-typescript/knexfile.ts
@@ -0,0 +1,12 @@
+import path from "path";
+
+module.exports = {
+  client: "sqlite3",
+  connection: { // onde ele vai se conectar ao banco de dados
+    filename: path.resolve(__dirname, "src", "database", "database.sqlite"),
+  },
+  migrations: { // onde estão as migrations
+    directory: path.resolve(__dirname, "src", "database", "migrations"),
+  },
+  useNullAsDefault: true,
+};
