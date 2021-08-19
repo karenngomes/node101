cf4820632c55fa19cb39098fc271b23343691a12
Karen Gomes <kngs@ic.ufal.br>
2021-08-19 14:53:17 -0300

    add seed

--- node-typescript/knexfile.ts
+++ node-typescript/knexfile.ts
@@ -8,5 +8,8 @@ module.exports = {
   migrations: { // onde estão as migrations
     directory: path.resolve(__dirname, "src", "database", "migrations"),
   },
+  seeds: {
+    directory: path.resolve(__dirname, "src", "database", "seeds"),
+  },
   useNullAsDefault: true,
 };
