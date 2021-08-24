2e19d86c6780aa86b8c098f13bbc459ddc954d56
Karen Gomes <kngs@ic.ufal.br>
2021-08-24 15:55:11 -0300

    add multer and cors types

--- node-typescript/src/server.ts
+++ node-typescript/src/server.ts
@@ -1,7 +1,7 @@
-import express from 'express';
+import express from "express";
 import cors from "cors";
-import path from 'path';
-import routes from './routes';
+import path from "path";
+import routes from "./routes";
 
 const app = express();
 
@@ -11,12 +11,12 @@ app.use(cors()); // allows any domain access this API
 //     origin: ['example-domain.com', 'another-domain-if-you-need-it.com']
 // }))
 
-app.use(express.json())
+app.use(express.json());
 
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333, () => {
-    console.log('Server started on port 3333')
+  console.log("Server started on port 3333");
 });
