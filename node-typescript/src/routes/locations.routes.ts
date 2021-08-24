3b5e8464b8316048f924229704550480015585c2
Karen Gomes <kngs@ic.ufal.br>
2021-08-24 14:53:59 -0300

    get locations by query params

--- node-typescript/src/routes/locations.routes.ts
+++ node-typescript/src/routes/locations.routes.ts
@@ -3,6 +3,43 @@ import knex from "../database/connection";
 
 const locationsRouter = Router();
 
+locationsRouter.get("/", async (request, response) => {
+  const { city, uf, items } = request.query;
+
+  const parsedItems = String(items)
+    .split(",")
+    .map((item) => {
+      return Number(item.trim());
+    });
+
+  const locations = await knex("locations")
+    .join("location_items", "locations.id", "=", "location_items.location_id")
+    .whereIn("location_items.item_id", parsedItems)
+    .where("city", String(city))
+    .where("uf", String(uf))
+    .distinct()
+    .select("locations.*");
+
+  return response.json(locations);
+});
+
+locationsRouter.get("/:id", async (request, response) => {
+  const { id } = request.params;
+
+  const location = await knex("locations").where("id", id).first();
+
+  if (!location) {
+    return response.status(400).json({ message: "Location not found" });
+  }
+
+  const items = await knex("items")
+    .join("location_items", "items.id", "=", "location_items.item_id")
+    .where("location_items.location_id", id)
+    .select("items.title");
+
+  return response.json({ location, items });
+});
+
 locationsRouter.post("/", async (request, response) => {
   const { name, email, whatsapp, latitude, longitude, city, uf, items } =
     request.body;
@@ -42,23 +79,6 @@ locationsRouter.post("/", async (request, response) => {
   await transaction.commit();
 
   return response.json({ id: locationId, ...location });
-});
-
-locationsRouter.get("/:id", async (request, response) => {
-  const { id } = request.params;
-
-  const location = await knex("locations").where("id", id).first();
-
-  if (!location) {
-    return response.status(400).json({ message: "Location not found" });
-  }
-
-  const items = await knex("items")
-    .join("location_items", "items.id", "=", "location_items.item_id")
-    .where("location_items.location_id", id)
-    .select("items.title");
-
-  return response.json({ location, items });
 });
 
 export default locationsRouter;
