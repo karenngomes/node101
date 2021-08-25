babc6f3048be1a6a365af9b9ff0747001b72efba
Karen Gomes <kngs@ic.ufal.br>
2021-08-25 19:57:22 -0300

    organize routes and controllers

--- node-typescript/src/routes/index.ts
+++ node-typescript/src/routes/index.ts
@@ -1,13 +1,47 @@
-import { Router } from 'express';
-import itemsRouter from './items.routes';
-import locationsRouter from './locations.routes';
+import { Router } from "express";
+import { celebrate, Joi } from "celebrate";
+import multer from "multer";
+import multerConfig from "../config/multer";
 import usersRouter from './users.routes';
 import sessionsRouter from './sessions.routes';
 
-const routes = Router();
+import LocationsController from "../controllers/LocationsController";
+import ItemsController from "../controllers/ItemsController";
 
-routes.use('/items', itemsRouter)
-routes.use('/locations', locationsRouter)
+const routes = Router();
+const upload = multer(multerConfig);
+
+const itemsController = new ItemsController();
+const locationsController = new LocationsController();
+
+routes.get("/items", itemsController.index);
+
+routes.get("/locations/:id", locationsController.show);
+routes.get("/locations", locationsController.index);
+
+routes.post(
+  "/locations",
+  upload.single("image"),
+  celebrate(
+    {
+      body: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().required().email(),
+        whatsapp: Joi.number().required(),
+        latitude: Joi.number().required(),
+        longitude: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      }),
+    },
+    {
+      abortEarly: false,
+    }
+  ),
+  locationsController.create
+);
+
 routes.use('/users', usersRouter)
 routes.use('/sessions', sessionsRouter)
 
